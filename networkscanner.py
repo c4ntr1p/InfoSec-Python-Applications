b32cc236d3e74600fb072f6b3f21f65d20ae464e
Tank <timothydaniel14@yahoo.com>
2024-03-05 00:09:00 -0500

    Large file update
    In short, lines 12-22 had to be moves some and I added for_loop and def_scan port function to allow the scan port function to communicate with the tkinter gui. Overall, took a good bit of research, the AI portion in my Visusal studio and we got it. We need to add a "cancel scan" input function... it got hung a few times and I had to ctrl alt delete

--- networkscanner.py
+++ networkscanner.py
@@ -1,20 +1,46 @@
-# Network Scanner for Python. I'd like to work on a GUI icon and display box so result happens outside of command line.
-# still look at which python interpreter will work for Manjaro. I would suggest python3, but if python is the best we can roll with that. 
+# Network Scanner for Python.
 
+# The goal Scan ports, tagets hosts and looks super cool.
+ 
+#!/usr/bin/env python3
+
+from tkinter import *
 from socket import *
 import time
-startTime = time.time()
+# So, the object here had to be moved around some
+def scan_ports():
+    target = entry.get() # retrieves the hostname
+    t_IP = gethostbyname(target) # resoolves host name to target ip
+    result_text.delete(1.0, END) # clears the widget of anything prior
+    result_text.insert(END, f'Starting scan on host: {t_IP}\n') # should display a messag to show start of the scan
 
-if __name__ == '__main__':
-   target = input('Enter the host to be scanned: ')
-   t_IP = gethostbyname(target)
-   print ('Starting scan on host: ', t_IP)
-   
-   for i in range(50, 500):
-      s = socket(AF_INET, SOCK_STREAM)
-      
-      conn = s.connect_ex((t_IP, i))
-      if(conn == 0) :
-         print ('Port %d: OPEN' % (i,))
-      s.close()
-print('Time taken:', time.time() - startTime)
+    for i in range(1, 1000): # the for i is to ensure scan exists for scanned ports 1-1000
+        s = socket(AF_INET, SOCK_STREAM) # creates TCP socket and address socket type
+        conn = s.connect_ex((t_IP, i))
+        if conn == 0: # if connection is succesful the conn == line will execute the scan, we could change the number if you want
+            result_text.insert(END, f'Port {i}: OPEN\n') # this is the line that shows the output of th scna in the widget
+        s.close()
+
+    result_text.insert(END, f'Time taken: {time.time() - startTime:.2f} seconds') # this just prints out the time result for the scan.
+
+# Created main loop window for desktop
+root = Tk()
+root.title('Network Scanner')
+
+# good ol widgets
+label = Label(root, text='Enter the host to be scanned:')
+label.pack(pady=10)
+
+entry = Entry(root)
+entry.pack(pady=10)
+# the actual scan button
+scan_button = Button(root, text='Scan Ports', command=scan_ports)
+scan_button.pack(pady=10)
+
+# loops with lines 12-22 for results in gui
+result_text = Text(root, height=10, width=50)
+result_text.pack(pady=10)
+
+# Start GUI event loop
+startTime = time.time()
+root.mainloop()
